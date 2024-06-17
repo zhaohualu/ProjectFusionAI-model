bb006ede5fa3a0506a433ade924f132889c35ccc
ahshen26 <77656572+ahshen26@users.noreply.github.com>
2024-06-17 11:05:30 -0400

    Add files via upload

---
+++ R/gbt_survival.R
@@ -0,0 +1,487 @@
+#' Gradient Boosted Trees using XGBoost for Survival Analysis
+#'
+#' @details See \url{https://radiant-rstats.github.io/docs/model/gbt.html} for an example in Radiant
+#'
+#' @param dataset Dataset
+#' @param time_var The time-to-event variable in the model
+#' @param status_var The event occurrence variable in the model
+#' @param evar Explanatory variables in the model
+#' @param lev Level to use as the first column in prediction output
+#' @param max_depth Maximum 'depth' of tree
+#' @param learning_rate Learning rate (eta)
+#' @param min_split_loss Minimal improvement (gamma)
+#' @param nrounds Number of trees to create
+#' @param min_child_weight Minimum number of instances allowed in each node
+#' @param subsample Subsample ratio of the training instances (0-1)
+#' @param early_stopping_rounds Early stopping rule
+#' @param nthread Number of parallel threads to use. Defaults to 12 if available
+#' @param wts Weights to use in estimation
+#' @param seed Random seed to use as the starting point
+#' @param data_filter Expression entered in, e.g., Data > View to filter the dataset in Radiant. The expression should be a string (e.g., "price > 10000")
+#' @param arr Expression to arrange (sort) the data on (e.g., "color, desc(price)")
+#' @param rows Rows to select from the specified dataset
+#' @param envir Environment to extract data from
+#' @param ... Further arguments to pass to xgboost
+#'
+#' @return A list with all variables defined in gbt as an object of class gbt
+#'
+#' @examples
+#' \dontrun{
+#' gbt_survival(lung, "time", "status", c("age", "sex", "ph.ecog")) %>% summary()
+#' gbt_survival(lung, "time", "status", c("age", "sex", "ph.ecog")) %>% str()
+#' }
+#' gbt_survival(
+#'   lung, "time", "status", c("age", "sex", "ph.ecog"), lev = "Yes",
+#'   early_stopping_rounds = 0, nthread = 1
+#' ) %>% summary()
+#' gbt_survival(
+#'   lung, "time", "status", c("age", "sex", "ph.ecog"),
+#'   early_stopping_rounds = 0, nthread = 1
+#' ) %>% str()
+#' gbt_survival(
+#'   lung, "time", "status", c("age", "sex", "ph.ecog"),
+#'   eval_metric = paste0("error@", 0.5 / 6), nthread = 1
+#' ) %>% str()
+#'
+#' @seealso \code{\link{summary.gbt}} to summarize results
+#' @seealso \code{\link{plot.gbt}} to plot results
+#' @seealso \code{\link{predict.gbt}} for prediction
+#'
+#' @importFrom xgboost xgboost xgb.importance
+#' @importFrom lubridate is.Date
+#' @importFrom survival survfit survdiff Surv
+#' @importFrom broom tidy
+#' @importFrom survC1 Cindex
+#'
+#' @export
+gbt_survival <- function(dataset, time_var, status_var, evar, lev = "",
+                         max_depth = 6, learning_rate = 0.3, min_split_loss = 0,
+                         min_child_weight = 1, subsample = 1,
+                         nrounds = 100, early_stopping_rounds = 10,
+                         nthread = 12, wts = "None", seed = NA,
+                         data_filter = "", arr = "", rows = NULL,
+                         envir = parent.frame(), ...) {
+  if (time_var %in% evar || status_var %in% evar) {
+    return("Time or status variable contained in the set of explanatory variables.\nPlease update model specification." %>%
+             add_class("gbt"))
+  }
+
+  vars <- c(time_var, status_var, evar)
+
+  if (is.empty(wts, "None")) {
+    wts <- NULL
+  } else if (is_string(wts)) {
+    wtsname <- wts
+    vars <- c(time_var, status_var, evar, wtsname)
+  }
+
+  df_name <- if (is_string(dataset)) dataset else deparse(substitute(dataset))
+  dataset <- get_data(dataset, vars, filt = data_filter, arr = arr, rows = rows, envir = envir) %>%
+    mutate_if(is.Date, as.numeric)
+  nr_obs <- nrow(dataset)
+
+  if (!is.empty(wts, "None")) {
+    if (exists("wtsname")) {
+      wts <- dataset[[wtsname]]
+      dataset <- select_at(dataset, .vars = base::setdiff(colnames(dataset), wtsname))
+    }
+    if (length(wts) != nrow(dataset)) {
+      return(
+        paste0("Length of the weights variable is not equal to the number of rows in the dataset (", format_nr(length(wts), dec = 0), " vs ", format_nr(nrow(dataset), dec = 0), ")") %>%
+          add_class("gbt")
+      )
+    }
+  }
+
+  not_vary <- colnames(dataset)[summarise_all(dataset, does_vary) == FALSE]
+  if (length(not_vary) > 0) {
+    return(paste0("The following variable(s) show no variation. Please select other variables.\n\n** ", paste0(not_vary, collapse = ", "), " **") %>%
+             add_class("gbt"))
+  }
+
+  gbt_input <- list(
+    max_depth = max_depth,
+    learning_rate = learning_rate,
+    min_split_loss = min_split_loss,
+    nrounds = nrounds,
+    min_child_weight = min_child_weight,
+    subsample = subsample,
+    early_stopping_rounds = early_stopping_rounds,
+    nthread = nthread
+  )
+
+  ## checking for extra args
+  extra_args <- list(...)
+  extra_args_names <- names(extra_args)
+  check_args <- function(arg, default, inp = gbt_input) {
+    if (!arg %in% extra_args_names) inp[[arg]] <- default
+    inp
+  }
+
+  gbt_input <- check_args("objective", "survival:cox")
+  gbt_input <- check_args("eval_metric", "cox-nloglik")
+
+  ## adding data
+  dtx <- model.matrix(~ . - 1, data = dataset[, evar, drop = FALSE])
+  dty <- dataset[[time_var]]
+
+  gbt_input <- c(gbt_input, list(data = dtx, label = dty), ...)
+
+  ## based on https://stackoverflow.com/questions/14324096/setting-seed-locally-not-globally-in-r/14324316#14324316
+  seed <- gsub("[^0-9]", "", seed)
+  if (!is.empty(seed)) {
+    if (exists(".Random.seed")) {
+      gseed <- .Random.seed
+      on.exit(.Random.seed <<- gseed)
+    }
+    set.seed(seed)
+  }
+
+  ## capturing the iteration history
+  output <- capture.output(model <<- do.call(xgboost::xgboost, gbt_input))
+
+  model$model <- dataset
+  ## gbt model object does not include the data by default
+
+  #rm(dataset, dty, dtx, rv) ## dataset not needed elsewhere
+  gbt_input$data <- gbt_input$label <- NULL
+
+  ## needed to work with prediction functions
+  check <- ""
+
+  as.list(environment()) %>% add_class(c("gbt", "model"))
+
+
+}
+
+#' Summary method for the gbt_survival function
+#'
+#' @details See \url{https://radiant-rstats.github.io/docs/model/gbt.html} for an example in Radiant
+#'
+#' @param object Return value from \code{\link{gbt_survival}}
+#' @param prn Print iteration history
+#' @param ... further arguments passed to or from other methods
+#'
+#' @examples
+#' result <- gbt_survival(
+#'   lung, "time", "status", c("age", "sex", "ph.ecog"),
+#'   early_stopping_rounds = 0, nthread = 1
+#' )
+#' summary(result)
+#' @seealso \code{\link{gbt_survival}} to generate results
+#' @seealso \code{\link{plot.gbt}} to plot results
+#' @seealso \code{\link{predict.gbt}} for prediction
+#'
+#' @export
+summary.gbt_survival <- function(object, prn = TRUE, ...) {
+  if (is.character(object)) {
+    return(object)
+  }
+  cat("(XGBoost) - Survival Analysis\n")
+  cat("Type                 : Survival Analysis")
+  cat("\nData                 :", object$df_name)
+  if (!is.empty(object$data_filter)) {
+    cat("\nFilter               :", gsub("\\n", "", object$data_filter))
+  }
+  if (!is.empty(object$arr)) {
+    cat("\nArrange              :", gsub("\\n", "", object$arr))
+  }
+  if (!is.empty(object$rows)) {
+    cat("\nSlice                :", gsub("\\n", "", object$rows))
+  }
+  cat("\nResponse variable    :", object$time_var)
+  cat("\nStatus variable      :", object$status_var)
+  cat("\nExplanatory variables:", paste0(object$evar, collapse = ", "), "\n")
+  if (length(object$wtsname) > 0) {
+    cat("Weights used         :", object$wtsname, "\n")
+  }
+  cat("Max depth            :", object$max_depth, "\n")
+  cat("Learning rate (eta)  :", object$learning_rate, "\n")
+  cat("Min split loss       :", object$min_split_loss, "\n")
+  cat("Min child weight     :", object$min_child_weight, "\n")
+  cat("Sub-sample           :", object$subsample, "\n")
+  cat("Nr of rounds (trees) :", object$nrounds, "\n")
+  cat("Early stopping rounds:", object$early_stopping_rounds, "\n")
+  if (length(object$extra_args)) {
+    extra_args <- deparse(object$extra_args) %>%
+      sub("list\\(", "", .) %>%
+      sub("\\)$", "", .) %>%
+      sub(" {2,}", " ", .)
+    cat("Additional arguments :", extra_args, "\n")
+  }
+  if (!is.empty(object$seed)) {
+    cat("Seed                 :", object$seed, "\n")
+  }
+
+  if (!is.empty(object$wts, "None") && (length(unique(object$wts)) > 2 || min(object$wts) >= 1)) {
+    cat("Nr obs               :", format_nr(sum(object$wts), dec = 0), "\n")
+  } else {
+    cat("Nr obs               :", format_nr(object$nr_obs, dec = 0), "\n")
+  }
+
+  if (isTRUE(prn)) {
+    cat("\nIteration history:\n\n")
+    ih <- object$output[c(-2, -3)]
+    if (length(ih) > 20) ih <- c(head(ih, 10), "...", tail(ih, 10))
+    cat(paste0(ih, collapse = "\n"))
+  }
+
+}
+#' Predict method for the gbt_survival function
+#'
+#' @details See \url{https://radiant-rstats.github.io/docs/model/gbt.html} for an example in Radiant
+#'
+#' @param object Return value from \code{\link{gbt_survival}}
+#' @param pred_data Provide the dataframe to generate predictions. The dataset must contain all columns used in the estimation
+#' @param pred_cmd Generate predictions using a command. For example, `age = seq(30, 60, 5)` would produce predictions for different ages. To add another variable, create a vector of prediction strings, e.g., c('age = seq(30, 60, 5)', 'sex = c("male", "female")')
+#' @param dec Number of decimals to show
+#' @param envir Environment to extract data from
+#' @param ... further arguments passed to or from other methods
+#'
+#' @examples
+#' result <- gbt_survival(lung, "time", "status", c("age", "sex", "ph.ecog"), nthread = 1)
+#' predict(result, pred_cmd = "age = seq(50, 70, 5)")
+#' predict(result, pred_data = lung) %>% head()
+#' @seealso \code{\link{gbt_survival}} to generate the result
+#' @seealso \code{\link{summary.gbt_survival}} to summarize results
+#'
+#' @export
+predict.gbt_survival <- function(object, pred_data = NULL, pred_cmd = "",
+                                 dec = 3, envir = parent.frame(), ...) {
+  if (is.character(object)) {
+    return(object)
+  }
+
+  # Ensure you have a name for the prediction dataset
+  if (is.data.frame(pred_data)) {
+    df_name <- deparse(substitute(pred_data))
+  } else {
+    df_name <- pred_data
+  }
+
+  pfun <- function(model, pred, se, conf_lev) {
+    # Ensure the factor levels in the prediction data are the
+    # same as in the data used for estimation
+    est_data <- model$model[, model$evar, drop = FALSE]
+    for (i in colnames(pred)) {
+      if (is.factor(est_data[[i]])) {
+        pred[[i]] <- factor(pred[[i]], levels = levels(est_data[[i]]))
+      }
+    }
+    # Explicitly construct the formula without using '.'
+    formula_str <- paste("~", paste(colnames(est_data), collapse = " + "), "- 1")
+    pred <- model.matrix(as.formula(formula_str), data = pred)
+
+    # Predict survival probabilities
+    pred_val <- try(sshhr(predict(model, newdata = pred)), silent = TRUE)
+    if (!inherits(pred_val, "try-error")) {
+      pred_val %<>% as.data.frame(stringsAsFactors = FALSE) %>%
+        select(1) %>%
+        set_colnames("Prediction")
+    }
+
+    pred_val
+  }
+
+  # Create a new dataframe for predictions if pred_cmd is provided
+  if (!is.null(pred_cmd) && pred_cmd != "") {
+    pred_data <- eval(parse(text = paste0("expand.grid(", paste(pred_cmd, collapse = ","), ")")), envir)
+  }
+
+  # Ensure pred_data contains all required variables
+  missing_vars <- setdiff(object$evar, colnames(pred_data))
+  if (length(missing_vars) > 0) {
+    stop(paste("The following variables are missing in pred_data:", paste(missing_vars, collapse = ", ")))
+  }
+
+  predict_model(object, pfun, "gbt_survival.predict", pred_data, pred_cmd, conf_lev = 0.95, se = FALSE, dec, envir = envir) %>%
+    set_attr("radiant_pred_data", df_name)
+}
+
+#' Print method for predict.gbt_survival
+#'
+#' @param x Return value from prediction method
+#' @param ... further arguments passed to or from other methods
+#' @param n Number of lines of prediction results to print. Use -1 to print all lines
+#'
+#' @export
+print.gbt_survival.predict <- function(x, ..., n = 10) {
+  print_predict_model(x, ..., n = n, header = "Gradient Boosted Trees - Survival Analysis")
+}
+
+#' Cross-Validated Gradient Boosted Trees using XGBoost for Survival Analysis
+#'
+#' @details See \url{https://radiant-rstats.github.io/docs/model/gbt.html} for an example in Radiant
+#'
+#' @param object The model object, either a previously fitted `gbt` object or an `xgb.Booster`
+#' @param K Number of folds for cross-validation
+#' @param repeats Number of times to repeat the cross-validation
+#' @param params List of parameters for the xgboost model
+#' @param nrounds Number of trees to create
+#' @param early_stopping_rounds Early stopping rule
+#' @param nthread Number of parallel threads to use. Defaults to 12 if available
+#' @param train Training data in xgboost DMatrix format
+#' @param type Type of model ("classification" or "regression")
+#' @param trace Logical flag for tracing progress
+#' @param seed Random seed to use as the starting point
+#' @param maximize Logical flag indicating whether to maximize the evaluation metric
+#' @param fun Custom evaluation function
+#' @param ... Further arguments to pass to xgboost
+#'
+#' @return A data frame with the cross-validation results
+#'
+#' @examples
+#' \dontrun{
+#' cv.gbt_survival(gbt_model, K = 5, repeats = 1, params = list(max_depth = 6, eta = 0.3))
+#' }
+#' @importFrom xgboost xgb.cv xgb.DMatrix
+#' @importFrom dplyr bind_rows
+#' @importFrom shiny incProgress withProgress
+#'
+#' @export
+#' Cross-Validated Gradient Boosted Trees using XGBoost for Survival Analysis
+#'
+#' @details See \url{https://radiant-rstats.github.io/docs/model/gbt.html} for an example in Radiant
+#'
+#' @param object The model object, either a previously fitted `gbt` object or an `xgb.Booster`
+#' @param K Number of folds for cross-validation
+#' @param repeats Number of times to repeat the cross-validation
+#' @param params List of parameters for the xgboost model
+#' @param nrounds Number of trees to create
+#' @param early_stopping_rounds Early stopping rule
+#' @param nthread Number of parallel threads to use. Defaults to 12 if available
+#' @param train Training data in xgboost DMatrix format
+#' @param type Type of model ("classification" or "regression")
+#' @param trace Logical flag for tracing progress
+#' @param seed Random seed to use as the starting point
+#' @param maximize Logical flag indicating whether to maximize the evaluation metric
+#' @param fun Custom evaluation function
+#' @param ... Further arguments to pass to xgboost
+#'
+#' @return A data frame with the cross-validation results
+#'
+#' @examples
+#' \dontrun{
+#' cv.gbt_survival(gbt_model, K = 5, repeats = 1, params = list(max_depth = 6, eta = 0.3))
+#' }
+#' @importFrom xgboost xgb.cv xgb.DMatrix
+#' @importFrom dplyr bind_rows
+#' @importFrom shiny incProgress withProgress
+#'
+#' @export
+cv.gbt_survival <- function(object, K = 5, repeats = 1, params = list(),
+                            nrounds = 500, early_stopping_rounds = 10, nthread = 12,
+                            train = NULL, type = "classification",
+                            trace = TRUE, seed = 1234, maximize = NULL, fun, ...) {
+  if (inherits(object, "gbt")) {
+    time_var <- object$time_var
+    status_var <- object$status_var
+    dataset <- object$model$model
+    evar <- object$evar
+
+    if (!is.character(evar)) {
+      stop("evar should be a character vector")
+    }
+
+    if (is.data.frame(dataset)) {
+      dtx <- model.matrix(~ . - 1, data = dataset[, evar, drop = FALSE])
+    } else {
+      stop("dataset should be a data frame")
+    }
+
+    dty <- dataset[[time_var]]
+    dstatus <- dataset[[status_var]]
+    train <- xgboost::xgb.DMatrix(data = dtx, label = dty)
+    type <- "survival"
+    objective <- "survival:cox"
+    params_base <- object$model$params
+  } else if (!inherits(object, "xgb.Booster")) {
+    stop("The model object does not seem to be a Gradient Boosted Tree")
+  } else {
+    if (!inherits(train, "xgb.DMatrix")) {
+      train <- eval(object$call[["data"]])
+    }
+    params_base <- object$params
+  }
+
+  if (!inherits(train, "xgb.DMatrix")) {
+    stop("Could not access data. Please use the 'train' argument to pass along a matrix created using xgboost::xgb.DMatrix")
+  }
+
+  params_base[c("nrounds", "nthread", "silent")] <- NULL
+  for (n in names(params)) {
+    params_base[[n]] <- params[[n]]
+  }
+  params <- params_base
+  if (is.null(maximize)) {
+    maximize <- params$maximize
+  }
+
+  if (missing(fun)) {
+    fun <- "cox-nloglik"
+  }
+
+  if (length(shiny::getDefaultReactiveDomain()) > 0) {
+    trace <- FALSE
+    incProgress <- shiny::incProgress
+    withProgress <- shiny::withProgress
+  } else {
+    incProgress <- function(...) {}
+    withProgress <- function(...) list(...)[["expr"]]
+  }
+
+  tf <- tempfile()
+  tune_grid <- expand.grid(params)
+  nitt <- nrow(tune_grid)
+  withProgress(message = "Running cross-validation (gbt_survival)", value = 0, {
+    out <- list()
+    for (i in seq_len(nitt)) {
+      cv_params <- tune_grid[i, ]
+      if (!is.null(cv_params$nrounds)) {
+        nrounds <- cv_params$nrounds
+        cv_params$nrounds <- NULL
+      }
+      if (trace) {
+        cat("Working on", paste0(paste(colnames(cv_params), "=", cv_params), collapse = ", "), "\n")
+      }
+      for (j in seq_len(repeats)) {
+        set.seed(seed)
+        sink(tf) ## avoiding messages from xgboost::xgb.cv
+        cv_params_tmp <- cv_params
+        for (nm in c("eval_metric", "maximize", "early_stopping_rounds", "nthread")) {
+          cv_params_tmp[[nm]] <- NULL
+        }
+        model <- try(xgboost::xgb.cv(
+          params = as.list(cv_params_tmp),
+          data = train,
+          nfold = K,
+          print_every_n = 500,
+          eval_metric = fun,
+          maximize = maximize,
+          early_stopping_rounds = early_stopping_rounds,
+          nrounds = nrounds,
+          nthread = nthread
+        ))
+        sink()
+        if (inherits(model, "try-error")) {
+          stop(model)
+        }
+        out[[paste0(i, "-", j)]] <- as.data.frame(c(
+          nrounds = nrounds, best_iteration = model$best_iteration,
+          model$evaluation_log[model$best_iteration, -1], cv_params
+        ))
+      }
+      incProgress(1 / nitt, detail = paste("\nCompleted run", i, "out of", nitt))
+    }
+  })
+
+  out <- bind_rows(out)
+  if (type == "classification") {
+    out[order(out[[5]], decreasing = TRUE), ]
+  } else {
+    out[order(out[[5]], decreasing = FALSE), ]
+  }
+}
+
+
+
